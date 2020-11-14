557cf4ddf5860ce08055a1920e960f7b55ed31d3
JACK <69492163+Jack-tg@users.noreply.github.com>
2020-11-14 11:45:38 +0530

    Update captions.py

--- plugins/captions.py
+++ plugins/captions.py
@@ -26,9 +26,11 @@ async def start(bot, update):
 
 @pyrogram.Client.on_message(pyrogram.Filters.photo) # @pyrogram.Client.on_message(pyrogram.Filters.document | Filters.video) set like this to trigger both or remove filters.document and add filters.video for video only
 async def old(bot, update):
-    stock = update.photo.file_ref
+    pyrogram.InputMediasticker(
+     media=sticker
+)
     await bot.send_sticker(
         chat_id=update.chat.id,
-        sticker=stock
+        sticker=media
         
     )
